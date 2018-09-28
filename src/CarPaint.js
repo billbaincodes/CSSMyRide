68d9244a642bb76dbc1c3ace04734218602b2237
bill <williamandrewbain@gmail.com>
2018-09-28 14:48:18 -0600

    removed unused import

--- src/CarPaint.js
+++ src/CarPaint.js
@@ -1,34 +1,34 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
 
-class CarPaint extends Component {
+// class CarPaint extends Component {
 
-  constructor(props){
-    super(props)
-    this.state = {carColor: "pink"}
-  }
+//   constructor(props){
+//     super(props)
+//     this.state = {carColor: "pink"}
+//   }
 
-  pinkPaint = () => {
-    this.setState({carColor: "pink"})
-  }
-  redPaint = () => {
-    this.setState({carColor: "red"})
-  }
-  silverPaint = () => {
-    this.setState({carColor: "silver"})
-  }
+//   pinkPaint = () => {
+//     this.setState({carColor: "pink"})
+//   }
+//   redPaint = () => {
+//     this.setState({carColor: "red"})
+//   }
+//   silverPaint = () => {
+//     this.setState({carColor: "silver"})
+//   }
 
-  render() {
-    return (
-      <div>
-        <h3>Your car is an ugly shade of {this.state.carColor}</h3>
-        <ul>Fix it with these paints:
-          <button onClick={this.pinkPaint}>pink</button>
-          <button onClick={this.redPaint}>red</button>
-          <button onClick={this.silverPaint}>silver</button>
-        </ul>
-      </div>
-    );
-  }
-}
+//   render() {
+//     return (
+//       <div>
+//         <h3>Your car is an ugly shade of {this.state.carColor}</h3>
+//         <ul>Fix it with these paints:
+//           <button onClick={this.pinkPaint}>pink</button>
+//           <button onClick={this.redPaint}>red</button>
+//           <button onClick={this.silverPaint}>silver</button>
+//         </ul>
+//       </div>
+//     );
+//   }
+// }
 
-export default CarPaint;
+// export default CarPaint;
